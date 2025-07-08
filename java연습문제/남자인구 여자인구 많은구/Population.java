a4c4ecbdaec0f3187e912683715819a3bc847640
dkldfjdlf <ha56035936@gmail.com>
2025-07-08 16:26:31 +0900

    Add files via upload

---
+++ java연습문제/남자인구 여자인구 많은구/Population.java
@@ -0,0 +1,26 @@
+package hajisu;
+
+public class Population {
+    String guName;
+    int guCode;
+    String yearMonth;
+    int totalPopulation;
+    int householdCount;
+    double populationPerHousehold;
+    int malePopulation;
+    int femalePopulation;
+
+    public Population(String guName, int guCode, String yearMonth,
+                      int totalPopulation, int householdCount,
+                      double populationPerHousehold,
+                      int malePopulation, int femalePopulation) {
+        this.guName = guName;
+        this.guCode = guCode;
+        this.yearMonth = yearMonth;
+        this.totalPopulation = totalPopulation;
+        this.householdCount = householdCount;
+        this.populationPerHousehold = populationPerHousehold;
+        this.malePopulation = malePopulation;
+        this.femalePopulation = femalePopulation;
+    }
+}
